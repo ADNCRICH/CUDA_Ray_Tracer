3f89ce567920425f4f1752fd2ce5625dda5cad20
ADNCRICH <p50629.2013x@gmail.com>
2024-05-31 07:02:49 +0700

    fast add~~~

---
+++ basic_CUDA/add.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void add(int n, float *x, float *y) {
+    for (int i = 0; i < n; i++)
+        y[i] = x[i] + y[i];
+}
+
+int main() {
+    int N = 1 << 20;
+
+    float *x = new float[N];
+    float *y = new float[N];
+
+    for (int i = 0; i < N; i++)
+        x[i] = 1.0f, y[i] = 2.0f;
+
+    add(N, x, y);
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++)
+        maxError = fmax(maxError, fabs(y[i] - 3.0f));
+    cout << "Max error: " << maxError << "\n";
+
+    delete[] x;
+    delete[] y;
+}
---
+++ basic_CUDA/add.cu
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+__global__ void add_one_thread(int n, float *x, float *y) {
+    int this_thread = threadIdx.x;  // current thread index = [0, second number]
+    int blockSize = blockDim.x;     // number of all thread in this block = second number
+    for (int i = this_thread; i < n; i += blockSize)
+        y[i] = x[i] + y[i];
+}
+
+__global__ void add_block_thread(int n, float *x, float *y) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;  // gridDim.x is number of block defined by first number
+    for (int i = index; i < n; i += stride)
+        y[i] += x[i];
+}
+
+// nvcc add.cu -o ../bin/add_cu
+// nsys nvprof ../bin/add_cu && rm report*.nsys-rep && rm report*.sqlite
+int main() {
+    int N = 1 << 24;
+
+    float *x, *y;
+    cudaMallocManaged(&x, N * sizeof(float));
+    cudaMallocManaged(&y, N * sizeof(float));
+
+    for (int i = 0; i < N; i++)
+        x[i] = 1.0f,
+        y[i] = 2.0f;
+
+    add_one_thread<<<1, 256>>>(N, x, y);
+
+    int block_size = 256;                               // number of thread in each block
+    int num_block = (N + block_size - 1) / block_size;  // round up number of block to support all workload
+
+    add_block_thread<<<num_block, block_size>>>(N, x, y);
+
+    cudaDeviceSynchronize();
+
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++)
+        maxError = fmax(maxError, fabs(y[i] - 6.0f));
+    cout << "Max error: " << maxError << "\n";
+
+    cudaFree(x);
+    cudaFree(y);
+}
